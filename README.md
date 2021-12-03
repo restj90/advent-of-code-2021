5470f4fac140d69cf2603c83197c2f7531da81ef
restj90 <marinescu.valentinc@yahoo.com>
2021-12-03 20:13:28 +0000

    add day 1 answer in nice format, extend utils

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # advent-of-code-2021
 My solutions to the Advent of Code 2021 puzzles.
+
+## prereqs
+miniconda
+
+## install
+```
+conda create --name aoc2021 python=3.9
+conda activate aoc2021
+conda install --file requirements.txt
+```
---
+++ utils.py
@@ -0,0 +1,27 @@
+import pathlib
+from aocd import get_data, submit
+
+# requires /inputs directory to be created
+# requires .token file (git-ignored) to be created
+#   how to get session token -> https://github.com/wimglenn/advent-of-code-wim/issues/1 
+ROOT=pathlib.Path(__file__).parent.resolve()
+INPUTS_DIR = ROOT/'inputs'
+AOC_SESSION=(ROOT/'.token').read_text().strip()
+
+def save_data_file(day, year):
+    file = INPUTS_DIR/f"day{str(day).rjust(2, '0')}.txt"
+    if not file.exists():
+        data = get_data(AOC_SESSION, day, year)
+        file.write_text(data.strip())
+    return file
+        
+def read_lines(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+def read_integers(file):
+    input_lines = read_lines(file)
+    return [int(i) for i in input_lines]
+
+def submit_answer(answer, part, day, year):
+    return submit(answer, part, day, year, AOC_SESSION)
